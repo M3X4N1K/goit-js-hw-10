9dd386e200a7d5f0c69a514a994824dc775f6fbf
Oleksiy_Lishchuk <ejik.alex9294@gmail.com>
2025-09-13 15:01:28 +0300

    fixed all 2

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
-// main.js
-document.addEventListener("DOMContentLoaded", async () => {
-  const loads = document.querySelectorAll("load[src]");
-  
-  for (const el of loads) {
-    const src = el.getAttribute("src");
-    const res = await fetch(src);
-    const html = await res.text();
-    const wrapper = document.createElement("div");
-    wrapper.innerHTML = html;
-    el.replaceWith(wrapper);
+// src/main.js
 
-    // Підключаємо скрипти з підвантаженого HTML
-    wrapper.querySelectorAll("script[type='module']").forEach(s => {
-      const newScript = document.createElement("script");
-      newScript.type = "module";
-      newScript.src = s.src;
-      document.body.appendChild(newScript);
-    });
-  }
-});
+// Підвантаження partials на всіх сторінках
+import './js/load-partials.js';
+
+// Підвантаження конкретного JS для сторінки
+if (document.querySelector('#datetime-picker')) {
+  import('./js/1-timer.js');
+} else if (document.querySelector('.form')) {
+  import('./js/2-snackbar.js');
+}
